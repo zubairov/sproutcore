b4a1c6da757992d5407c483ad7ba5f49716f353b
Colin Campbell <colin@strobecorp.com>
2011-01-31 20:52:25 -0330

    Initial rewrite of SC.SceneView so it uses SC.View#animate instead of its own animation code

--- frameworks/desktop/views/scene.js
+++ frameworks/desktop/views/scene.js
@@ -30,7 +30,23 @@
   @since SproutCore 1.0
 */
 SC.SceneView = SC.ContainerView.extend(
-  /** @scope SC.SceneView.prototype */ {
+/** @scope SC.SceneView.prototype */ {
+
+
+  // ..........................................................
+  // Properties
+  // 
+
+  /**
+    The currently showing scene.  Changing this property will cause the 
+    scene view to transition to the new scene.  If you set this property to 
+    null, an empty string, or a non-existant scene, then the scene will appear
+    empty.
+
+    @property {String|SC.View}
+    @default null
+  */
+  nowShowing: null,
 
   /**
     Array of scene names.  Scenes will slide on and off screen in the order
@@ -42,163 +58,137 @@ SC.SceneView = SC.ContainerView.extend(
     augment this array as you like.
     
     @property {Array}
+    @default ['master', 'detail']
   */
   scenes: ['master', 'detail'],
 
   /**
-    The currently showing scene.  Changing this property will cause the 
-    scene view to transition to the new scene.  If you set this property to 
-    null, an empty string, or a non-existant scene, then the scene will appear
-    empty.
-  */
-  nowShowing: null,
-  
-  /**
     Speed of transition.  Should be expressed in msec.
+    
+    @property {Number}
+    @default 200
   */
   transitionDuration: 200,
   
-  _state: 'NO_VIEW', // no view
-
-  /** @private
-  
-    Whenever called to change the content, save the nowShowing state and 
-    then animate in by adjusting the layout.
+  /**
+    The ease to use on the transition.
     
+    @property {String}
+    @default 'ease-out'
+  */
+  transitionTiming: 'ease-out',
+
+
+  // ..........................................................
+  // Content Handling
+  // 
+
+  /**
+    Whenever called to change the content, save the nowShowing state and
+    then animate in by adjusting the layout.
   */
   replaceContent: function(content) {
-    if (content && this._state===this.READY) this.animateScene(content);
-    else this.replaceScene(content);
-    return this ;
-  },
-
-  /** @private
-  
-    Invoked whenever we just need to swap the scenes without playing an
-    animation.
-  */
-  replaceScene: function(newContent) {
-    var oldContent = this._targetView,
-        layout     = this.STANDARD_LAYOUT,
-        scenes     = this.get('scenes'),
-        idx        = scenes ? scenes.indexOf(this.get('nowShowing')) : -1;
-
-    // cleanup animation here too..
-    this._targetView = newContent ;
-    this._targetIndex  = idx;
-    
-    if (this._timer) this._timer.invalidate();
-    this._leftView = this._rightView = this._start = this._end = null;
-    this._timer = null;
-    
-    
-    this.removeAllChildren();
-
-    if (oldContent) oldContent.set('layout', layout);
-    if (newContent) newContent.set('layout', layout);
-    
-    if (newContent) this.appendChild(newContent);
-    this._state = newContent ? this.READY : this.NO_VIEW ;
-  },
-
-  /** @private
-  
-    Invoked whenever we need to animate in the new scene.
-  */
-  animateScene: function(newContent) {
-    var oldContent = this._targetView,
-        outIdx     = this._targetIndex,
-        scenes     = this.get('scenes'),
-        inIdx      = scenes ? scenes.indexOf(this.get('nowShowing')) : -1,
-        layout;
-
-    if (outIdx<0 || inIdx<0 || outIdx===inIdx) {
-      return this.replaceScene(newContent);
-    }
-
-    this._targetView = newContent ;
-    this._targetIndex = inIdx; 
-    
-    // save some info needed for animation
-    if (inIdx > outIdx) {
-      this._leftView  = oldContent;
-      this._rightView = newContent;
-      this._target    = -1;
-    } else {
-      this._leftView  = newContent ;
-      this._rightView = oldContent ;
-      this._target    = 1 ;
-    }
-
-    // setup views
-    this.removeAllChildren();
-
-    if (oldContent) this.appendChild(oldContent)
-    if (newContent) this.appendChild(newContent);
-
-    // setup other general state
-    this._start   = Date.now();
-    this._end     = this._start + this.get('transitionDuration');
-    this._state   = this.ANIMATING;
-    this.tick();
-  },
-
-  /** @private - called while the animation runs.  Compute the new layout for
-    the left and right views based on the portion completed.  When we finish
-    call replaceScene().
-  */
-  tick: function() {  
-    this._timer = null ; // clear out
-    
-    var now    = Date.now(),
-        pct    = (now-this._start)/(this._end-this._start),
-        target = this._target,
-        left   = this._leftView,
-        right  = this._rightView,
-        layout, adjust;
-        
-    if (pct<0) pct = 0;
-    
-    // if we're done or the view is no longer visible, just replace the 
-    // scene.
-    if (!this.get('isVisibleInWindow') || (pct>=1)) {
-      return this.replaceScene(this._targetView);
-    }
-
-    // ok, now let's compute the new layouts for the two views and set them
-    layout = SC.clone(this.get('frame'));
-    adjust = Math.floor(layout.width * pct);
-    
-    // set the layout for the views, depending on the direction
-    if (target>0) {
-      layout.left = 0-(layout.width-adjust);
-      left.set('layout', layout);
-
-      layout = SC.clone(layout);
-      layout.left = adjust ;
-      right.set('layout', layout);
-      
-    } else {
-      layout.left = 0-adjust ;
-      left.set('layout', layout);
-      
-      layout = SC.clone(layout);
-      layout.left = layout.width-adjust;
-      right.set('layout', layout);
-    }
-
-    this._timer = this.invokeLater(this.tick, 20);
+    if (content && this._state === SC.SceneView.READY) this._animateScene(content);
+    else this._replaceScene(content, YES);
     return this;
   },
-  
 
-  // states for view animation
-  NO_VIEW: 'NO_VIEW',
-  ANIMATING: 'ANIMATING',
-  READY: 'READY',
 
-  /** @private - standard layout assigned to views at rest */
-  STANDARD_LAYOUT: { top: 0, left: 0, bottom: 0, right: 0 }
-  
-  
+  // ..........................................................
+  // Internal Support
+  // 
+
+  /** @private
+    Possible values:
+      - SC.SceneView.NO_VIEW
+      - SC.SceneView.ANIMATING
+      - SC.SceneView.READY
+    
+    @property {String}
+    @default SC.SceneView.NO_VIEW
+  */
+  _state: 'NO_VIEW',
+
+  /** @private
+    Invoked whenever we just need to swap the scenes without playing an
+    animation.
+    
+    @param {SC.View} newContent The content to use as the new scene
+    @param {Boolean} removeChildren If true, removes the children currently appended
+  */
+  _replaceScene: function(newContent, removeChildren) {
+    var layout = SC.SceneView.STANDARD_LAYOUT,
+        scenes = this.get('scenes'),
+        idx = scenes ? scenes.indexOf(this.get('nowShowing')) : -1;
+    
+    if (removeChildren) this.removeAllChildren();
+    
+    if (newContent) {
+      newContent.set('layout', layout);
+      this.appendChild(newContent);
+    }
+    
+    this._targetView = newContent;
+    
+    this._state = newContent ? SC.SceneView.READY : SC.SceneView.NO_VIEW;
+  },
+
+  /** @private
+    Invoked whenever we need to animate in the new scene.
+    
+    @param {SC.View} newContent The content to use as the new scene
+  */
+  _animateScene: function(newContent) {
+    var oldContent = this._targetView,
+        outIdx = this._targetIndex,
+        scenes = this.get('scenes'),
+        inIdx = scenes ? scenes.indexOf(this.get('nowShowing')) : -1,
+        info = {duration: this.get('transitionDuration') / 1000, timing: this.get('transitionTiming')},
+        layout = SC.clone(this.get('frame')),
+        that = this, left;
+    
+    if (outIdx < 0 || inIdx < 0 || outIdx === inIdx || !newContent) {
+      return this._replaceScene(newContent, YES);
+    }
+    
+    delete layout.x;
+    delete layout.y;
+    
+    oldContent.adjust(layout);
+    
+    if (inIdx < outIdx) {
+      left = layout.width;
+      
+      layout = SC.clone(layout);
+      layout.left = -layout.width;
+    } else {
+      left = -layout.width;
+      
+      layout = SC.clone(layout);
+      layout.left = layout.width;
+    }
+    
+    newContent.adjust(layout);
+    
+    this.appendChild(newContent);
+    
+    this._targetView = newContent;
+    this._targetIndex = inIdx;
+    
+    oldContent.invokeLater('animate', 1, 'left', left, info, function() {
+      that.removeChild(oldContent);
+    });
+    
+    newContent.invokeLater('animate', 1, 'left', 0, SC.clone(info), function() {
+      that._replaceScene(newContent, NO);
+    });
+  }
+
 });
+
+SC.SceneView.NO_VIEW = 'NO_VIEW';
+SC.SceneView.ANIMATING = 'ANIMATING';
+SC.SceneView.READY = 'READY';
+
+/** @private - standard layout assigned to views at rest */
+SC.SceneView.STANDARD_LAYOUT = {bottom: 0, left: 0, right: 0, top: 0};
